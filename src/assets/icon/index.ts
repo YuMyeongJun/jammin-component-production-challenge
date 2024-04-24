f342474ebfb23c0628429d374a18a99c9ee59647
MyeongJun-Yu <y1207cc@gmail.com>
2024-04-25 00:24:58 +0900

    icon 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,5 +1,16 @@
-export { default as IcClose } from './ic_close.svg?react';
-export { default as ICCheckboxChecked } from './ic_checkbox_checked.svg?react';
-export { default as ICCheckboxRounded } from './ic_checkbox_rounded.svg?react';
-export { default as ICCheckboxOutlineBlankRounded } from './ic_checkbox_outline_blank_rounded.svg?react';
-export { default as ICCheckboxIdenterminateRounded } from './ic_checkbox_indeterminate_rounded.svg?react';
+import icImg from "@icons/ic_img.svg";
+import icImgAddHover from "@icons/ic_img_add_hover.svg";
+import icImgHover from "@icons/ic_img_hover.svg";
+import icImgNotFound from "@icons/ic_img_notfound.svg";
+
+export { default as IcCalendar } from "@icons/ic_calendar.svg?react";
+export { default as IcArrowLeftDouble } from "@icons/ic_chevron_double_left.svg?react";
+export { default as IcArrowRightDouble } from "@icons/ic_chevron_double_right.svg?react";
+export { default as IcArrowLeft } from "@icons/ic_chevron_left.svg?react";
+export { default as IcArrowRight } from "@icons/ic_chevron_right.svg?react";
+export { default as IcSearch } from "@icons/ic_search.svg?react";
+export { default as IcSearchActive } from "@icons/ic_search_active.svg?react";
+export { default as IcSortAsc } from "@icons/ic_sort_asc.svg?react";
+export { default as IcSortDesc } from "@icons/ic_sort_desc.svg?react";
+export { default as IcSortNone } from "@icons/ic_sort_none.svg?react";
+export { icImg, icImgAddHover, icImgHover, icImgNotFound };
